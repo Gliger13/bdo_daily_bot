d6ced44b2f57b7c1f8c89051a0ca0647c50fdca8
Gliger13 <zanevskiyandrey@gmail.com>
2020-06-05 01:11:44 +0300

    Update_2.0.0/Fix: Generete and hide new secrets

--- bot/settings/secrets.py
+++
@@ -1,8 +0,0 @@
-DEBUG_TOKEN = 'Njk4NTgwNzE5OTc4NDE0MTIw.XpTNvQ._oS0lZlCKxkbd82NCQinvdI5cjY'
-DEBUG_TOKEN_ID = 698580719978414120
-
-# old NjgwNTE2MjUyMjY3MTg0Mjg1.XpTNlQ.dVLpEXEQz_jI6K_mb1bEtWLgB_0
-PRODUCTION_TOKEN = 'NjgwNTE2MjUyMjY3MTg0Mjg1.XtLDog.nai8s86-leh1B_KNNBean6pXAck'
-PRODUCTION_BOT_ID = 680516252267184285
-
-DB_STRING = "mongodb+srv://GligerOwner:Vfntvfnbrf2303@cluster0-kytaw.mongodb.net/test?retryWrites=true&w=majority"
