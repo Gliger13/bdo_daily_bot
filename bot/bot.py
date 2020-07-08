43df78022cc5243591e937226ca17bf2f691b8a6
Gliger13 <zanevskiyandrey@gmail.com>
2020-07-09 01:06:09 +0300

    Update: process errors that can raised in commands
    Former-commit-id: 6106febff00ff7ec5b77dfac8b2dbce09be97514
    Former-commit-id: ae2f8a1ace359ac70389f050efde2e3df2d517b2 [formerly b2eb089cc4b339c0cd8cdb02c70581b26f8b000b]
    Former-commit-id: 0aeb75a743164cc20922c27e33c2c13d92b5ab23

--- bot/bot.py
+++ bot/bot.py
@@ -8,7 +8,8 @@ from discord.ext import commands
 from settings import settings
 
 # Uncomment this to use pyinstaller
-from commands import events, base, raid_manager, fun
+# from commands import events, base, admin, fun, statistics
+# from commands.raid_manager import creation, save_load, registration, joining, overview
 
 # Initialization logger
 from settings import logger
