994767a8ac44e669b476d984308ba6ee68b60316
gliger13 <zanevskiyandrey@gmail.com>
2021-03-15 01:42:44 +0300

    Refactor/Docs: Add docstrings and clean code.
    Former-commit-id: f22527c338771adbf98356b35c01f3d065371f03
    Former-commit-id: e4f9910daf14e6ae55715cce58b54f7e0e3c00ef [formerly 5ea64913f22a2b1c4c7809f30bcec3eea8a520a1]
    Former-commit-id: d1a54ec96c08024dafbce2ab64eafaf3fe35ef0f

--- bot/instruments/database/user_collection.py
+++ bot/instruments/database/user_collection.py
@@ -1,155 +1,247 @@
+"""Contains the class for working with the user database collection."""
 import logging
+from typing import List, Dict, Any
 
-from instruments.database.database import Database, Error
+from motor.motor_asyncio import AsyncIOMotorCollection
+
+from instruments.database.database import Database
 from instruments.tools import MetaSingleton
 from settings import settings
 
 module_logger = logging.getLogger('my_bot')
 
 
-class UserExists(Error):
-    """
-    Exception raised when user exist in database
+class UserExists(Exception):
+    """Exception raised when user exist in database."""
 
-    Attributes:
-        expression -- input expression in which the error occurred
-    """
+    def __init__(self, discord_id: int):
+        """
+        :param discord_id: User discord id.
+        :type discord_id: int
+        """
+        error_msg = f"A user with discord id - {discord_id} already exists in user database collection."
+        module_logger.error(error_msg)
+        super().__init__(error_msg)
 
-    def __init__(self, expression):
-        self.expression = expression
 
+class UserNotExists(Exception):
+    """Exception raised when user not exist in database."""
 
-class UserNotExists(Error):
-    """
-    Exception raised when user not exist in database
-
-    Attributes:
-        expression -- input expression in which the error occurred
-    """
-    def __init__(self, expression):
-        self.expression = expression
+    def __init__(self, discord_id: int):
+        """
+        :param discord_id: User discord id.
+        :type discord_id: int
+        """
+        error_msg = f"User with discord id - {discord_id} not exists in the user database collection."
+        module_logger.error(error_msg)
+        super().__init__(error_msg)
 
 
 class UserCollection(metaclass=MetaSingleton):
-    _collection = None
+    """Responsible for working with the user MongoDB collection."""
+    _collection = None  # Contain database user collection
 
     @property
-    def collection(self):
+    def collection(self) -> AsyncIOMotorCollection:
+        """
+        Responsible for providing user collection.
+
+        Responsible for providing user collection. If this collection exists, it returns it.
+        If the collection does not exist, then it is create and provide.
+
+        :return: User database collection.
+        :rtype: AsyncIOMotorCollection
+        """
         if not self._collection:
             self._collection = Database().database[settings.USER_COLLECTION]
-            module_logger.debug(f'Collection {settings.USER_COLLECTION} connected.')
+            module_logger.debug(f"Collection {settings.USER_COLLECTION} connected.")
         return self._collection
 
     async def is_user_exist(self, discord_id: int) -> bool:
-        user_post = await self.collection.find_one({"discord_id": discord_id})
-        return True if user_post else False
+        """
+        Checks the existence of a user in the database by its discord id.
+
+        :param discord_id: User discord id.
+        :type discord_id: int
+        :return: The existence or lack of a user in the database.
+        :rtype: bool
+        """
+        user_post = await self.collection.find_one({'discord_id': discord_id})
+        return bool(user_post)
 
     async def get_user_by_id(self, discord_id: int) -> dict or None:
-        return await self.collection.find_one(
-            {"discord_id": discord_id},
-        )
+        """
+        Returns the user's document by its discord id from the database collection.
+
+        :param discord_id: User discord id.
+        :type discord_id: int
+        :return: Search results.
+        :rtype: dict
+        """
+        return await self.collection.find_one({'discord_id': discord_id})
 
     async def find_user_by_nickname(self, nickname: str) -> dict or None:
+        """
+        Returns the user's document by its discord id from the database collection.
+
+        :param nickname: User game nickname.
+        :type nickname: str
+        :return: Search results.
+        :rtype: dict
+        """
         return await self.collection.find_one({'nickname': nickname})
 
-    async def reg_user(self, discord_id: int, discord_user: str, nickname: str):
-        if await self.is_user_exist(discord_id):
-            raise UserExists('A user with these credentials already exists')
+    async def register_user(self, discord_id: int, discord_user: str, nickname: str):
+        """
+        Registers user data in the user database collection.
 
-        post = {
-            "discord_id": discord_id,
+        :param discord_id: User discord id.
+        :type discord_id: int
+        :param discord_user: User discord name.
+        :type discord_user: str
+        :param nickname: User game nickname.
+        :type nickname: str
+        """
+        if await self.is_user_exist(discord_id):
+            raise UserExists(discord_id)
+
+        new_user_post = {
+            'discord_id': discord_id,
             'discord_user': discord_user,
             'nickname': nickname,
             'entries': 0,
         }
-        self.collection.insert_one(post)
+        self.collection.insert_one(new_user_post)
 
     async def re_register_user(self, discord_id: int, discord_user: str, nickname: str):
-        user_post = await self.get_user_by_id(discord_id)
+        """
+        Reregister user data by its discord id in the user database collection.
 
-        if user_post:
-            user_entries = entries if (entries := user_post.get("entries")) else 0
-            post = {'$set': {
+        :param discord_id: User discord id.
+        :type discord_id: int
+        :param discord_user: User discord name.
+        :type discord_user: str
+        :param nickname: User game nickname.
+        :type nickname: str
+        """
+        old_user_document = await self.get_user_by_id(discord_id)
+
+        if old_user_document:
+            user_entries = old_user_document.get("entries", 0)
+
+            new_user_document = {'$set': {
+                'discord_id': discord_id,
                 'discord_user': discord_user,
                 'nickname': nickname,
                 'entries': user_entries,
-                }
-            }
-            await self.collection.update_one(user_post, post)
+            }}
+            await self.collection.update_one(old_user_document, new_user_document)
         else:
-            await self.reg_user(discord_id, discord_user, nickname)
+            await self.register_user(discord_id, discord_user, nickname)
 
     async def user_joined_raid(self, discord_id: int):
+        """
+        Write to the user database collection that the user visited the raid.
+
+        :param discord_id: User discord id.
+        :type discord_id: int
+        """
         await self.collection.find_one_and_update(
-            {
-                'discord_id': discord_id
-            },
-            {
-                '$inc': {'entries': 1}
-            }
+            {'discord_id': discord_id},
+            {'$inc': {'entries': 1}}
         )
 
     async def user_leave_raid(self, discord_id: int):
+        """
+        Write to the user database collection that the user leave the raid.
+
+        :param discord_id: User discord id.
+        :type discord_id: int
+        """
         await self.collection.find_one_and_update(
-            {
-                'discord_id': discord_id
-            },
-            {
-                '$inc': {'entries': -1}
-            }
+            {'$and': [{'discord_id': discord_id}, {'entries': {'$gt': 0}}]},
+            {'$inc': {'entries': -1}}
         )
 
     async def set_notify_off(self, discord_id: int):
+        """
+        Writes to the database that the user disable bot notifications.
+
+        :param discord_id: User discord id.
+        :type discord_id: int
+        """
         await self.collection.find_one_and_update(
-            {
-                'discord_id': discord_id
-            },
-            {
-                '$set': {'not_notify': True}
-            }
+            {'discord_id': discord_id},
+            {'$set': {'not_notify': True}}
         )
 
     async def set_notify_on(self, discord_id: int):
+        """
+        Writes to the database that the user enable bot notifications.
+
+        :param discord_id: User discord id.
+        :type discord_id: int
+        """
         await self.collection.find_one_and_update(
-            {
-                'discord_id': discord_id
-            },
-            {
-                '$set': {'not_notify': False}
-            }
+            {'discord_id': discord_id},
+            {'$set': {'not_notify': False}}
         )
 
     async def not_notify_status(self, discord_id: int) -> bool:
-        user_post = await self.get_user_by_id(discord_id)
+        """
+        Returns the status of user notifications.
 
-        if user_post:
-            return True if user_post.get('not_notify') else False
-        else:
-            raise UserNotExists(f"User with discord id {discord_id} not exists in the collection")
+        :param discord_id: User discord id.
+        :type discord_id: int
+        :return: Status of user notifications.
+        :rtype: bool
+        """
+        user_document = await self.get_user_by_id(discord_id)
+
+        if not user_document:
+            raise UserNotExists(discord_id)
+
+        return user_document.get('not_notify', False)
 
     async def set_first_notification(self, discord_id: int):
+        """
+        Set the fact of receipt the first notification by the user in the user database collection.
+
+        :param discord_id: User discord id.
+        :type discord_id: int
+        """
         await self.collection.find_one_and_update(
-            {
-                'discord_id': discord_id
-            },
-            {
-                '$set': {'first_notification': True}
-            }
+            {'discord_id': discord_id},
+            {'$set': {'first_notification': True}}
         )
 
-    async def first_notification_status(self, discord_id: int):
-        user_post = await self.get_user_by_id(discord_id)
+    async def first_notification_status(self, discord_id: int) -> bool:
+        """
+        Return the fact of receipt the first notification by the user in the user database collection.
 
-        if user_post:
-            return True if user_post.get('first_notification') else False
-        else:
-            raise UserNotExists(f"User with discord id {discord_id} not exists in the collection")
+        :param discord_id: User discord id.
+        :type discord_id: int
+        :return: Fact of receipt the first notification.
+        :rtype: bool
+        """
+        user_document = await self.get_user_by_id(discord_id)
 
-    async def get_users_id(self, nicknames_list: [str]) -> [dict]:
-        post = await self.collection.find(
-            {
-                'nickname': {'$in': nicknames_list}
-            },
+        if not user_document:
+            raise UserNotExists(discord_id)
+
+        return user_document.get('first_notification', False)
+
+    async def get_users_by_nicknames(self, nicknames_list: List[str]) -> List[Dict[str, Any]]:
+        """
+        Returns a list of users documents based on their game nicknames.
+
+        :param nicknames_list: Users game nicknames.
+        :type nicknames_list: List[str]
+        :return: Users documents.
+        :rtype: List[Dict[str, Any]]:
+        """
+        users_documents_cursor = await self.collection.find(
+            {'nickname': {'$in': nicknames_list}},
             {
                 'discord_id': 1,
                 'not_notify': 1,
@@ -157,4 +249,4 @@ class UserCollection(metaclass=MetaSingleton):
                 '_id': 0,
             }
         )
-        return list(post)
+        return list(users_documents_cursor)
