279cae4b5a075dfafd24bf238d79b60cd8e3cac1
gliger13 <zanevskiyandrey@gmail.com>
2021-03-15 02:45:52 +0300

    Docs: add docs to database and database manager
    Former-commit-id: 3981446f8f7bf90a012ae4a809244bd9ce51293c
    Former-commit-id: 3ad1d353c5d8996aac86282ceb7f61ccd7b349e7 [formerly 6ef65c70a3d30adb5c8bf03d0ab1bba6624439f4]
    Former-commit-id: 467de2e6bab9bc83dd9b9c6ebec7d6cd364c52ba

--- bot/instruments/database/database.py
+++ bot/instruments/database/database.py
@@ -1,6 +1,7 @@
+"""Contain class which is a MongoDB wrapper."""
 import logging
 
-from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorCollection
+from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorDatabase
 
 from instruments.tools import MetaSingleton
 from settings import settings
@@ -8,18 +9,24 @@ from settings import settings
 module_logger = logging.getLogger('my_bot')
 
 
-class Error(Exception):
-    """Base class for exceptions in this module."""
-    pass
-
-
 class Database(metaclass=MetaSingleton):
     """
-    Connect with Mongo Database
-    """
-    _cluster = None
+    Responsible for providing database.
 
-    def _connect(self) -> AsyncIOMotorCollection:
+    Responsible for responding the database and connecting to the database.
+    """
+    _cluster = None  # MongoDB cluster
+
+    def _connect(self) -> AsyncIOMotorDatabase:
+        """
+        Responsible for providing the database.
+
+        Responsible for providing the database. If this database exists, it returns it.
+        If the database does not exist, then it is connect and provide.
+
+        :return: Mongo database.
+        :rtype: AsyncIOMotorDatabase
+        """
         if not self._cluster:
             module_logger.debug('Initialisation database.')
             self._cluster = AsyncIOMotorClient(settings.BD_STRING)[settings.CLUSTER_NAME]
@@ -27,5 +34,11 @@ class Database(metaclass=MetaSingleton):
         return self._cluster
 
     @property
-    def database(self) -> AsyncIOMotorCollection:
+    def database(self) -> AsyncIOMotorDatabase:
+        """
+        Mongo database.
+
+        :return: Mongo database.
+        :rtype: AsyncIOMotorDatabase
+        """
         return self._connect()
--- bot/instruments/database/manager.py
+++ bot/instruments/database/manager.py
@@ -1,9 +1,16 @@
+"""Contain the database manager that manages all database collections."""
 from instruments.database.captain_collection import CaptainCollection
 from instruments.database.settings_collection import SettingsCollection
 from instruments.database.user_collection import UserCollection
 
 
 class DatabaseManager:
+    """
+    Responsible for database collection management.
+
+    Responsible for database collection management. Provides access to database collections.
+    """
+
     def __init__(self):
         self.user = UserCollection()
         self.captain = CaptainCollection()
